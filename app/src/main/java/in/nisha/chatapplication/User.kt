bf55c9e3ad81e4c1db53531cfed518b14dc650ab
Nishabankar <nishab2607@gmail.com>
2022-09-03 23:09:03 +0530

    first commit

---
+++ app/src/main/java/in/nisha/chatapplication/User.kt
@@ -0,0 +1,16 @@
+package `in`.nisha.chatapplication
+
+class User {
+    var name: String? = null
+    var email: String? = null
+    var Uid: String? = null
+
+    constructor(){}
+
+    constructor(name: String?,email: String?,Uid: String){
+        this.name = name
+        this.email = email
+        this.Uid = Uid
+    }
+
+}
